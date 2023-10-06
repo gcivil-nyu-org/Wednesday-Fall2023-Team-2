70704bbaafdab043d6e566be31b7553458fa48b2
Jimmy <jimmy@Jimmys-Air.lan>
2023-10-05 20:14:28 -0400

    Fixing template folder structure

--- parkrowd/parkrowd/settings.py
+++ parkrowd/parkrowd/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'parkrowd.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR,'templates')],
+        'DIRS': [os.path.join(BASE_DIR,'parkrowd/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
