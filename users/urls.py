96754b63db05070730a991784a85b071b665e770
jkcny <90436243+jkcny@users.noreply.github.com>
2023-10-16 13:24:17 -0400

    Profile edit (#36)
    * add a page for profile edit
    
    * add existing username check
    
    * fix formatting

--- users/urls.py
+++ users/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('register/', views.UserRegisterView.as_view(), name='register'),
     path('login/', views.UserLoginView.as_view(), name='login'),
     path('profile/<str:username>/', views.UserProfileView.as_view(), name='profile'),
+    path('profile-edit/<str:username>/', views.UserProfileEditView.as_view(), name='profile_edit'),
     path('logout/', views.UserLogoutView.as_view(), name='logout'),
     path('welcome/', views.UserWelcomeView.as_view(), name='welcome')
 ]
