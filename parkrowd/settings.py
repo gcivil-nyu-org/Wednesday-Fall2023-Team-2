bb8a64c2e4cd9ce4cf37787635b68715401c694e
NigelLu <nigellu@outlook.com>
2023-10-19 16:33:57 -0400

    revise: re-organized items in settings.py

--- parkrowd/settings.py
+++ parkrowd/settings.py
@@ -8,56 +8,40 @@ from django.urls import reverse_lazy
 
 from utils.aws_utils import get_linux_ec2_private_ip
 
-
+# region: prep
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 # load the .env file
 load_dotenv(dotenv_path=os.path.join(BASE_DIR, ".env"), verbose=False)
+# endregion: prep
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-fwhre62z62nwjg@ft0(-6^pt6@aaa$p+ha0xdsl$qpk5j0sc#n"
 
-# SECURITY WARNING: don't run with debug turned on in production!
+# region: django default (likely remain unchanged)
+# load secret key from OS env to keep it secret
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
+
+# use OS env to control this
 DEBUG = os.getenv("PROD") == "false"
 
-# allowed hosts settings
-ALLOWED_HOSTS = [
-    "parkrowd-env.eba-spjjw3yh.us-west-2.elasticbeanstalk.com"
-    if os.getenv("PROD") != "false"
-    else "127.0.0.1"
-]
-# ElasticBeanstalk healthcheck sends requests with host header = internal ip
-# So we detect if we are in elastic beanstalk,
-# and add the instances private ip address
-private_ip = get_linux_ec2_private_ip()
-if private_ip:
-    ALLOWED_HOSTS += [private_ip]
+# * Internationalization
+# https://docs.djangoproject.com/en/4.2/topics/i18n/
+LANGUAGE_CODE = "en-us"
+TIME_ZONE = "America/New_York"
+# TODO: not sure why translation and localizing time is needed
+# USE_TZ = True
+# USE_I18N = True
 
+# * Default primary key field type
+# https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-# Application definition
-INSTALLED_APPS = [
-    "django.contrib.auth",
-    "django.contrib.admin",
-    "users.apps.UsersConfig",
-    "django.contrib.sessions",
-    "django.contrib.messages",
-    "django.contrib.staticfiles",
-    "django.contrib.contenttypes",
-]
+# * Session expiry default setting
+SESSION_EXPIRE_AT_BROWSER_CLOSE = True
 
-MIDDLEWARE = [
-    "django.middleware.security.SecurityMiddleware",
-    "django.contrib.sessions.middleware.SessionMiddleware",
-    "django.middleware.common.CommonMiddleware",
-    "django.middleware.csrf.CsrfViewMiddleware",
-    "django.contrib.auth.middleware.AuthenticationMiddleware",
-    "django.contrib.messages.middleware.MessageMiddleware",
-    "django.middleware.clickjacking.XFrameOptionsMiddleware",
-]
+# wsgi application
+WSGI_APPLICATION = "parkrowd.wsgi.application"
 
-ROOT_URLCONF = "parkrowd.urls"
-
+# * Template engine and template directories
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
@@ -74,42 +58,9 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = "parkrowd.wsgi.application"
 
-
-# Database
-# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-DATABASES = {
-    "default": {
-        "PORT": os.getenv("RDS_PORT"),
-        "USER": os.getenv("RDS_USERNAME"),
-        "HOST": os.getenv("RDS_HOSTNAME"),
-        "NAME": os.getenv("RDS_DB_NAME"),
-        "PASSWORD": os.getenv("RDS_PASSWORD"),
-        "ENGINE": "django.db.backends.postgresql",
-    }
-}
-
-# Custom User Model for Authorization
-AUTH_USER_MODEL = "users.User"
-
-AUTHENTICATION_BACKENDS = [
-    "users.backends.EmailOrUsernameAuthenticationBackend",
-]
-
-LOGIN_URL = LOGOUT_REDIRECT_URL = reverse_lazy("users:login")
-
-# session expiry default setting
-SESSION_EXPIRE_AT_BROWSER_CLOSE = True
-
-# Changes messages to be based on SessionStorage, not CookieStorage
-# To fix bug with messages still appearing on screen, after logging out
-MESSAGE_STORAGE = "django.contrib.messages.storage.session.SessionStorage"
-
-
-# Password validation
+# *Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
-
 AUTH_PASSWORD_VALIDATORS = [
     {
         "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
@@ -124,30 +75,75 @@ AUTH_PASSWORD_VALIDATORS = [
         "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
+# endregion: django default (likely remain unchanged)
 
+# region: production related (likely to be changed)
+ROOT_URLCONF = "parkrowd.urls"
 
-# Internationalization
-# https://docs.djangoproject.com/en/4.2/topics/i18n/
+# * Allowed hosts settings
+ALLOWED_HOSTS = [
+    "127.0.0.1"
+    if os.getenv("PROD") == "false"
+    else "parkrowd-env.eba-spjjw3yh.us-west-2.elasticbeanstalk.com"
+]
+# ElasticBeanstalk healthcheck sends requests with host header = internal ip
+# So we detect if we are in elastic beanstalk,
+# and add the instances private ip address
+private_ip = get_linux_ec2_private_ip()
+if private_ip:
+    ALLOWED_HOSTS += [private_ip]
 
-LANGUAGE_CODE = "en-us"
+# * Custom User Model for Authorization
+AUTH_USER_MODEL = "users.User"
 
-TIME_ZONE = "America/New_York"
+LOGIN_URL = reverse_lazy("users:login")
+LOGOUT_REDIRECT_URL = reverse_lazy("users:login")
+AUTHENTICATION_BACKENDS = ["users.backends.EmailOrUsernameAuthenticationBackend"]
 
-# TODO: not sure why translation and localizing time is needed
-# USE_TZ = True
-# USE_I18N = True
+# Changes messages to be based on SessionStorage, not CookieStorage
+# To fix bug with messages still appearing on screen, after logging out
+MESSAGE_STORAGE = "django.contrib.messages.storage.session.SessionStorage"
 
-
-# Static files (CSS, JavaScript, Images)
+# * Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
-
 STATIC_URL = "/static/"
 STATIC_ROOT = "static"
 # Image File Setup
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 MEDIA_URL = "/media/"
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
+# * Application definition
+INSTALLED_APPS = [
+    "django.contrib.auth",
+    "django.contrib.admin",
+    "users.apps.UsersConfig",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "django.contrib.contenttypes",
+]
 
-DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+# * Middleware definition
+MIDDLEWARE = [
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+]
+
+# * Database
+# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+DATABASES = {
+    "default": {
+        "PORT": os.getenv("RDS_PORT"),
+        "USER": os.getenv("RDS_USERNAME"),
+        "HOST": os.getenv("RDS_HOSTNAME"),
+        "NAME": os.getenv("RDS_DB_NAME"),
+        "PASSWORD": os.getenv("RDS_PASSWORD"),
+        "ENGINE": "django.db.backends.postgresql",
+    }
+}
+# endregion: production related (likely to be changed)
