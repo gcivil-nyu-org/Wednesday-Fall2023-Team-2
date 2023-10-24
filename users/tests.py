7e74140291025c176ad02e507eb2e96a585aab32
Jimmy <jimmy@Jimmys-Air.lan>
2023-10-24 12:27:20 -0400

    DRYed and added Remember Me check

--- users/tests.py
+++ users/tests.py
@@ -1,33 +1,46 @@
 from django.test import TestCase
 from django.urls import reverse
-from .models import User
 from django.contrib.auth.forms import AuthenticationForm
+
+import dateutil.parser
+
+from django.utils import timezone
+from datetime import timedelta
+
+from .models import User
+from .views import SESSION_COOKIE_EXPIRATION
+
+
+USERNAME = "parkrowd"
+EMAIL = "parkrowd@gmail.com"
+PASSWORD = "iLikeParking123!"
+DUMMY_PASSWORD = "noParkingForYou"
+
+LOGIN_PATH_NAME = "users:login"
+WELCOME_PATH_NAME = "users:welcome"
+PROFILE_PATH_NAME = "users:profile"
+
+LOGIN_TEMPLATE = "login.html"
+REGISTER_TEMPLATE = "register.html"
+WELCOME_TEMPLATE = "welcome.html"
+PROFILE_TEMPLATE = "profile.html"
 
 
 class LoginTests(TestCase):
     def setUp(self):
         """creates user with sample credentials"""
-        self.username = "parkrowd"
-        self.email = "parkrowd@gmail.com"
-        self.password = "iLikeParking123!"
-
-        self.login_path_name = "users:login"
-        self.welcome_path_name = "users:welcome"
-        self.profile_path_name = "users:profile"
-
-        self.template = "login.html"
 
         self.user = User.objects.create_user(
-            username=self.username, email=self.email, password=self.password
+            username=USERNAME, email=EMAIL, password=PASSWORD
         )
 
     def test_login_view(self):
         """checks if login page returns a 200 Status Code
         and the template 'login.html' is used
         """
-        response = self.client.get(reverse(self.login_path_name))
+        response = self.client.get(reverse(LOGIN_PATH_NAME))
         self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, "login.html")
+        self.assertTemplateUsed(response, LOGIN_TEMPLATE)
 
     def test_successful_login_with_username(self):
         """checks if login page correctly redirects (302 Status Code)
@@ -35,12 +48,10 @@ class LoginTests(TestCase):
         with correct username and password
         """
         response = self.client.post(
-            reverse(self.login_path_name),
-            {"username": self.email, "password": self.password},
+            reverse(LOGIN_PATH_NAME),
+            {"username": USERNAME, "password": PASSWORD},
         )
-        self.assertRedirects(
-            response, reverse(self.profile_path_name, args=[self.username])
-        )
+        self.assertRedirects(response, reverse(PROFILE_PATH_NAME, args=[USERNAME]))
 
     def test_successful_login_with_email(self):
         """checks if login page correctly redirects (302 Status Code)
@@ -48,13 +59,36 @@ class LoginTests(TestCase):
         with correct email and password
         """
         response_with_username = self.client.post(
-            reverse(self.login_path_name),
-            {"username": self.username, "password": self.password},
+            reverse(LOGIN_PATH_NAME),
+            {"username": EMAIL, "password": PASSWORD},
         )
         self.assertRedirects(
             response_with_username,
-            reverse(self.profile_path_name, args=[self.username]),
+            reverse(PROFILE_PATH_NAME, args=[USERNAME]),
         )
+
+    def test_remember_me_login(self):
+        """checks if login page correctly redirects (302 Status Code)
+        to the profile page of the user
+        and the session cookie expiration is set to be > 1 day
+        """
+
+        response = self.client.post(
+            reverse(LOGIN_PATH_NAME),
+            {"username": USERNAME, "password": PASSWORD, "remember_me": "on"},
+        )
+        self.assertRedirects(
+            response,
+            reverse(PROFILE_PATH_NAME, args=[USERNAME]),
+        )
+
+        session_cookie = response.cookies.get("sessionid")
+        self.assertTrue(session_cookie)
+
+        session_expiry = dateutil.parser.parse(session_cookie.get("expires"))
+        minimum_expiration = timezone.now() + timedelta(days=1)
+
+        self.assertGreater(session_expiry.timestamp(), minimum_expiration.timestamp())
 
     def test_unsuccessful_login(self):
         """checks if login page has the following...
@@ -63,11 +97,9 @@ class LoginTests(TestCase):
         form contains 'credential invalid' type of message
         """
 
-        dummy_password = "noParkingForYou"
-
         response = self.client.post(
-            reverse("users:login"),
-            {"username": self.username, "password": dummy_password},
+            reverse(LOGIN_PATH_NAME),
+            {"username": USERNAME, "password": DUMMY_PASSWORD},
         )
         self.assertEqual(response.status_code, 200)
         form = response.context["form"]
