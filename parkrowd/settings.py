f07c143fe0d57e1a7ea8aafb8ed2eec793570c65
Andrew Giordano <andrewhg414@hotmail.com>
2023-10-31 14:12:09 -0400

    File Path Fix

--- parkrowd/settings.py
+++ parkrowd/settings.py
@@ -99,6 +99,7 @@ TEMPLATES = [
 
 
 
+# Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 DATABASES = {
     "default": {
@@ -171,11 +172,13 @@ MESSAGE_STORAGE = "django.contrib.messages.storage.session.SessionStorage"
 
 # * Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
+
 STATIC_URL = "/static/"
-STATIC_ROOT = "static"
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
 # Image File Setup
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
-MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL= '/media/'
 
 # * Email functionality
 EMAIL_USE_TLS = True
