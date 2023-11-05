4ac38476d19b2acb37abbcc8eeb2428b064a90f2
Jun Kui Chen <jc11224@nyu.edu>
2023-11-05 04:27:51 -0500

    add street parking data

--- map/models.py
+++ map/models.py
@@ -3,8 +3,12 @@ from django.db import models
 
 # Create your models here.
 class ParkingSpace(models.Model):
-    dca_license_number = models.CharField(max_length=200, primary_key=True)
+    parking_spot_id = models.CharField(max_length=200, primary_key=True)
     address_zip = models.CharField(max_length=200)
     longitude = models.CharField(max_length=200)
     latitude = models.CharField(max_length=200)
-    business_name = models.CharField(max_length=200)
+    parking_spot_name = models.CharField(max_length=200)
+    type = models.CharField(max_length=200, default='unknown')
+    borough = models.CharField(max_length=200, default='unknown')
+    detail = models.CharField(max_length=200, default='unknown')
+    operation_hours = models.CharField(max_length=200, default='unknown')
