cf12121746968a3d378855f314dafa4c73b76585
Jun Kui Chen <jc11224@nyu.edu>
2023-11-07 00:15:36 -0500

    add different color for street parking

--- map/views.py
+++ map/views.py
@@ -24,6 +24,7 @@ class MapView(View):
         context = {
             "GOOGLE_MAPS_API_KEY": settings.GOOGLE_MAPS_API_KEY,
             "parking_space": parking_space,
+            "GOOGLE_MAP_ID": settings.GOOGLE_MAP_ID,
         }
         return render(request, self.template_name, context)
 
