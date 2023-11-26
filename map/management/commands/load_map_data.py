a26e7226debe1426c36caddef97f16aed7c4b9f0
NigelLu <nigellu@outlook.com>
2023-11-25 20:17:54 -0500

    improve: make data loading scripts incremental

--- map/management/commands/load_map_data.py
+++ map/management/commands/load_map_data.py
@@ -2,6 +2,7 @@ import os
 import csv
 from tqdm import tqdm
 from pathlib import Path
+from django.db.utils import IntegrityError
 from django.core.management.base import BaseCommand
 
 from map.models import ParkingSpace
@@ -11,24 +12,28 @@ csv_file_path = os.path.join(BASE_DIR, "data/concat_result.csv")
 
 
 class Command(BaseCommand):
-    """create an admin user"""
+    """load latest data and merge into database"""
 
     def handle(self, *args, **options):
         print(f"csv file path is: {csv_file_path}")
+
         count = 0
         with open(csv_file_path, "r") as file:
             reader = csv.DictReader(file)
             for row in tqdm(reader):
-                ParkingSpace.objects.create(
-                    type=row["type"],
-                    detail=row["detail"],
-                    borough=row["borough"],
-                    latitude=row["latitude"],
-                    longitude=row["longitude"],
-                    address_zip=row["address_zip"],
-                    operation_hours=row["operation_hours"],
-                    parking_spot_id=row["parking_spot_id"],
-                    parking_spot_name=row["parking_spot_name"],
-                )
-                count += 1
+                try:
+                    ParkingSpace.objects.create(
+                        type=row["type"],
+                        detail=row["detail"],
+                        borough=row["borough"],
+                        latitude=row["latitude"],
+                        longitude=row["longitude"],
+                        address_zip=row["address_zip"],
+                        operation_hours=row["operation_hours"],
+                        parking_spot_id=row["parking_spot_id"],
+                        parking_spot_name=row["parking_spot_name"],
+                    )
+                    count += 1
+                except IntegrityError:
+                    print(f"Skipping Parking Space Record[{row['parking_spot_id']}]")
         print(f"{count} records added!")
