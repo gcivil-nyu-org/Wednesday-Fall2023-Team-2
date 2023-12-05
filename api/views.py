d47d55d1330bca594137329943cf3450c8a4551d
NigelLu <nigellu@outlook.com>
2023-12-05 18:29:38 -0500

    feat: users can put watch on parking spots

--- api/views.py
+++ api/views.py
@@ -1,16 +1,20 @@
 from rest_framework import status
 from django.utils import timezone
+from django.template import loader
 from django.http import HttpRequest
-from django.shortcuts import get_object_or_404
 from rest_framework import generics
 from haversine import haversine, Unit
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from django.contrib.auth import get_user_model
+from django.shortcuts import get_object_or_404
+from django.core.mail import EmailMultiAlternatives
 from rest_framework.permissions import IsAuthenticated
+from django.contrib.sites.shortcuts import get_current_site
 from rest_framework.authentication import SessionAuthentication
 
 from map.models import ParkingSpace, OccupancyHistory
-from users.models import Post, Comment
+from users.models import Post, Comment, UserWatchedParkingSpace
 from .serializers import ParkingSpaceSerializer, PostSerializer
 
 from better_profanity import profanity
@@ -18,6 +22,7 @@ from better_profanity import profanity
 profanity.load_censor_words()
 # Custom swear words can be added to this array
 custom_badwords = ["bullshittery", "bitchy"]
+User = get_user_model()
 
 
 class ParkingSpaceNearCenterAPIView(generics.ListAPIView):
@@ -84,6 +89,10 @@ class ParkingSpaceChangeOccupancyAPIView(APIView):
     permission_classes = [IsAuthenticated]
     authentication_classes = [SessionAuthentication]
 
+    extra_email_context = {"site_name": "Parkrowd"}
+    email_template_name = "api/parkingspace_status_update_email_template.html"
+    subject_template_name = "api/parkingspace_status_update_email_subject_template.txt"
+
     def post(self, request: HttpRequest) -> Response:
         """handles post requests to API endpoint above
 
@@ -137,6 +146,31 @@ class ParkingSpaceChangeOccupancyAPIView(APIView):
             parking_space.save()
             history.save()
 
+            # send emails to users that put a watch on this spot
+            current_site = get_current_site(request)
+            email_field_name = User.get_email_field_name()
+            user_watches = UserWatchedParkingSpace.objects.filter(
+                parking_space=parking_space, threshold__gte=occupancy_percent
+            )
+            domain = current_site.domain
+
+            for record in user_watches:
+                user_email = getattr(record.user, email_field_name)
+                context = {
+                    "user": record.user,
+                    "email": user_email,
+                    "domain": domain,
+                    "protocol": "https" if self.request.is_secure() else "http",
+                    **(self.extra_email_context or {}),
+                }
+                self.send_mail(
+                    self.subject_template_name,
+                    self.email_template_name,
+                    context,
+                    None,
+                    user_email,
+                )
+
             return Response(
                 {"message": "Occupancy percent updated successfully."},
                 status=status.HTTP_200_OK,
@@ -153,6 +187,30 @@ class ParkingSpaceChangeOccupancyAPIView(APIView):
                 {"message": f"An error occurred: {str(e)}"},
                 status=status.HTTP_500_INTERNAL_SERVER_ERROR,
             )
+
+    def send_mail(
+        self,
+        subject_template_name,
+        email_template_name,
+        context,
+        from_email,
+        to_email,
+        html_email_template_name=None,
+    ):
+        """
+        Send a django.core.mail.EmailMultiAlternatives to `to_email`.
+        """
+        subject = loader.render_to_string(subject_template_name, context)
+        # Email subject *must not* contain newlines
+        subject = "".join(subject.splitlines())
+        body = loader.render_to_string(email_template_name, context)
+
+        email_message = EmailMultiAlternatives(subject, body, from_email, [to_email])
+        if html_email_template_name is not None:
+            html_email = loader.render_to_string(html_email_template_name, context)
+            email_message.attach_alternative(html_email, "text/html")
+
+        email_message.send()
 
 
 class ParkingSpacePostsAPIView(generics.ListAPIView):
@@ -198,3 +256,26 @@ class ParkingSpaceAddCommentAPIView(APIView):
         new_comment.save()
 
         return Response("Comment created!", 200)
+
+
+class AddWatchOnParkingSpaceAPIView(APIView):
+    model = UserWatchedParkingSpace
+    permission_classes = [IsAuthenticated]
+    authentication_classes = [SessionAuthentication]
+
+    def post(self, request: HttpRequest) -> Response:
+        user = request.user
+        try:
+            parkingSpace = ParkingSpace.objects.get(
+                parking_spot_id=request.data["parking_spot_id"]
+            )
+        except ParkingSpace.DoesNotExist:
+            return Response("Invalid parking spot id", 400)
+
+        threshold = request.data["threshold"] if request.data["threshold"] else None
+        newUserWatchedParkingSpace = UserWatchedParkingSpace(
+            user=user, parkingSpace=parkingSpace, threshold=threshold
+        )
+        newUserWatchedParkingSpace.save()
+
+        return Response(f"Watch on spot {parkingSpace['parking_spot_id']} added", 200)
--- users/admin.py
+++ users/admin.py
@@ -2,10 +2,11 @@
 """
 from django.contrib import admin
 
-from .models import User, UserVerification, Post, Comment
+from .models import User, UserVerification, Post, Comment, UserWatchedParkingSpace
 
 # Register your models here.
 admin.site.register(User)
 admin.site.register(Post)
 admin.site.register(Comment)
 admin.site.register(UserVerification)
+admin.site.register(UserWatchedParkingSpace)
